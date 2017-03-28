87b776e59eb1bea57940e73792293e25f145eef5
zhaoyao <zhaoyao@howzhi.com>
2017-03-28 18:40:19 +0800

    杨辉三角

---
+++ YHTriangle.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>杨辉三角</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    </head>
+    <header>
+            <h1><center>杨辉三角</center></h1>
+    </header>
+</html>
